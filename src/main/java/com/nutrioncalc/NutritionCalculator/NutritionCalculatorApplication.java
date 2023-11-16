e9fb1806570cdabae6a1c8c9a13f44dab1f48497
Anatolii <omelchenko20@gmail.com>
2023-11-16 17:22:06 +0200

    Initial commit

---
+++ src/main/java/com/nutrioncalc/NutritionCalculator/NutritionCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.nutrioncalc.NutritionCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutrioncalc/NutritionCalculator/NutritionCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrioncalc.NutritionCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
